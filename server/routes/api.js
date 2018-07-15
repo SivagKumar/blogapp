e0754df21dfa9ee96b0498ed24e3448e3bbea7ab
SivagKumar <sivakumar1592@gmail.com>
2018-07-15 16:09:27 -0400

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const router= express.Router();
 const mongoose= require('mongoose');
-const db= "Your db link";
+const db= " PLEASE ENTER YOUR DB LINK";
 const article= require('../models/article');
 
 mongoose.Promise = global.Promise;
